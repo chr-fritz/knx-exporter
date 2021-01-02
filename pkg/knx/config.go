cfd70cb6a07ae333ce13e74617bf6fc0f01837b3
Christian Fritz <mail@chr-fritz.de>
2021-01-02 22:37:18 +0100

    Adds information about the knx connection to the config.

--- pkg/knx/config.go
+++ pkg/knx/config.go
@@ -1,15 +1,45 @@
 package knx
 
 import (
+	"fmt"
+	"strings"
 	"time"
 )
 
 // Config defines the structure of the configuration file which defines which
 // KNX Group Addresses were mapped into prometheus metrics.
 type Config struct {
+	Connection Connection `json:",omitempty"`
 	// MetricsPrefix is a short prefix which will be added in front of the actual metric name.
 	MetricsPrefix  string
 	AddressConfigs map[GroupAddress]GroupAddressConfig
+}
+
+type Connection struct {
+	Type     ConnectionType
+	Endpoint string
+}
+
+type ConnectionType string
+
+const Tunnel = ConnectionType("Tunnel")
+const Router = ConnectionType("Router")
+
+func (t ConnectionType) MarshalJSON() ([]byte, error) {
+	return []byte("\"" + t + "\""), nil
+}
+
+func (t *ConnectionType) UnmarshalJSON(data []byte) error {
+	str := string(data)
+	switch strings.ToLower(str) {
+	case "tunnel":
+		*t = Tunnel
+	case "router":
+		*t = Router
+	default:
+		return fmt.Errorf("invalid connection type given: \"%s\"", str)
+	}
+	return nil
 }
 
 // GroupAddressConfig defines all information to map a KNX group address to a prometheus metric.
