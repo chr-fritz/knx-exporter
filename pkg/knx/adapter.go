f1824d10cf0bf702ce267201056d237d6e82f182
Christian Fritz <mail@chr-fritz.de>
2021-02-13 20:07:03 +0100

    Adds custom logger for github.com/vapourismo/knx-go/knx package.

--- pkg/knx/adapter.go
+++ pkg/knx/adapter.go
@@ -2,7 +2,11 @@ package knx
 
 //go:generate mockgen -destination=adapterMocks_test.go -package=knx -source=adapter.go
 
-import "github.com/vapourismo/knx-go/knx"
+import (
+	"github.com/sirupsen/logrus"
+	"github.com/vapourismo/knx-go/knx"
+	"github.com/vapourismo/knx-go/knx/util"
+)
 
 // GroupClient is a super interface for the knx.GroupClient interface to also export the Close() function.
 type GroupClient interface {
@@ -17,4 +21,14 @@ type DPT interface {
 	Unpack(data []byte) error
 	Unit() string
 	String() string
+}
+
+func init() {
+	util.Logger = logger{}
+}
+
+type logger struct{}
+
+func (l logger) Printf(format string, args ...interface{}) {
+	logrus.Debugf(format, args...)
 }
