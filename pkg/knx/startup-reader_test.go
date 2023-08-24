c2c9ac138552e632528730ae19ad38dc5f69777d
simon mittelberger <>
2023-08-24 21:26:36 +0200

    add test for complete StartupReader

--- pkg/knx/startup-reader_test.go
+++ pkg/knx/startup-reader_test.go
@@ -2,8 +2,13 @@ package knx
 
 import (
 	"testing"
+	"time"
 
+	gomock "github.com/golang/mock/gomock"
+	"github.com/prometheus/client_golang/prometheus"
 	"github.com/stretchr/testify/assert"
+	"github.com/vapourismo/knx-go/knx"
+	"github.com/vapourismo/knx-go/knx/cemi"
 )
 
 func Test_getMetricsToRead(t *testing.T) {
@@ -59,4 +64,36 @@ func Test_getMetricsToRead(t *testing.T) {
 			assert.Equal(t, tt.want, got)
 		})
 	}
+}
+
+func TestStartupReader(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	groupClient := NewMockGroupClient(ctrl)
+	mockSnapshotHandler := NewMockMetricSnapshotHandler(ctrl)
+	messageCounter := prometheus.NewCounterVec(prometheus.CounterOpts{}, []string{"direction", "processed"})
+
+	config, err := ReadConfig("fixtures/readConfig.yaml")
+
+	assert.NoError(t, err)
+
+	groupClient.EXPECT().Send(knx.GroupEvent{
+		Command: knx.GroupRead, Source: cemi.NewIndividualAddr3(2, 0, 1), Destination: cemi.NewGroupAddr3(0, 0, 1),
+	}).Times(1)
+	groupClient.EXPECT().Send(knx.GroupEvent{
+		Command: knx.GroupRead, Source: cemi.NewIndividualAddr3(2, 0, 1), Destination: cemi.NewGroupAddr3(0, 0, 2),
+	}).Times(1)
+	groupClient.EXPECT().Send(knx.GroupEvent{
+		Command: knx.GroupRead, Source: cemi.NewIndividualAddr3(2, 0, 1), Destination: cemi.NewGroupAddr3(0, 0, 3),
+	}).Times(1)
+	groupClient.EXPECT().Send(knx.GroupEvent{
+		Command: knx.GroupRead, Source: cemi.NewIndividualAddr3(2, 0, 1), Destination: cemi.NewGroupAddr3(0, 0, 4),
+	}).Times(0)
+
+	s := NewStartupReader(config, groupClient, mockSnapshotHandler, messageCounter)
+	s.Run()
+	time.Sleep(2000 * time.Millisecond)
+
+	s.Close()
 }
